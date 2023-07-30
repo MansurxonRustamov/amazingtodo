39ebf6b45ad05100e2b6b60ca98630b9838fce3a
Mansurxon <mansurxonr2007@gmail.com>
2023-07-30 18:49:28 +0500

    Add To With Pin code

--- script.js
+++ script.js
@@ -1,19 +1,66 @@
 let controls = document.querySelectorAll(".controls")
-	info = document.querySelectorAll('.item')
 	infoClass = document.querySelectorAll('.info')
 	createT = document.querySelector(".plus")
 	createNone = document.querySelector(".createTobox")
 	btnAdd = document.querySelector(".btnAdd")
 	addInput = document.querySelector(".createNone input")
 	resultTobox = document.querySelector(".resultTobox")
+	clearAll = document.querySelector(".clear button")
+setTimeout(()=>{
+	document.querySelector(".loaderBox1").style.display='none'
+},2000)
+swal.setActionValue({ confirm: 'Text from input' })
 
 btnAdd.addEventListener("click",()=>{
 	ritem1 = document.querySelectorAll(".item")
 	taskinfo = {id:ritem1.length+1, name:addInput.value.trim(), status:"false"}
 	if (addInput.value.trim() != ""){
 		localStorage.setItem(`task${ritem1.length !=0 ? ritem1.length+1 : 1}`, JSON.stringify(taskinfo))
-		ritem2()
-	}else{alert("iltimos yozing")}
+		swal({
+			timer:2000,
+ 			 icon: "success",
+ 			 text:"List Yaratildi"
+		});
+		setTimeout(()=>{
+			ritem2()
+		},1000)
+		addInput.value=""
+		lenI1()
+
+	}else{
+		swal({
+			timer:2000,
+ 			 icon: "error",
+ 			 text:"Avval Yozish talab qilinadi"
+		});
+	}
+})
+addInput.addEventListener("keyup", (e)=>{
+	if(e.key=="Enter"){
+		ritem1 = document.querySelectorAll(".item")
+	taskinfo = {id:ritem1.length+1, name:addInput.value.trim(), status:"false"}
+	if (addInput.value.trim() != ""){
+		localStorage.setItem(`task${ritem1.length !=0 ? ritem1.length+1 : 1}`, JSON.stringify(taskinfo))
+		swal({
+			timer:2000,
+ 			 icon: "success",
+ 			 text:"List Yaratildi"
+		});
+		setTimeout(()=>{
+			ritem2()
+		},1000)
+		addInput.value=""
+		lenI1()
+
+
+	}else{
+		swal({
+			timer:2000,
+ 			 icon: "error",
+ 			 text:"Avval Yozish talab qilinadi"
+		});
+	}
+	}else{}
 })
 
 
@@ -37,6 +84,7 @@ function ritem2() {
 	datJs = JSON.parse(localStorage.getItem(`task${ritem.length !=0 ? ritem.length+1 : 1}`))
 	ritemDiv.innerHTML = `<input type="checkbox" ${datJs.status !="false"? "checked" : ""}  id="task${ritem.length+1}" class="checkmark"> <label for="${ritem.length+1}" >${datJs.name.slice(0, 26)}...</label> <div class="controls ">
 					<span class="fa-light fa-copy copy"></span>
+					<div class="fa-light fa-pen pen" id="pen"></div>
 					<i class="fa-light fa-trash-can-xmark delete"></i>
 				</div>`
 	resultTobox.appendChild(ritemDiv)
@@ -49,6 +97,7 @@ function ritem3() {
 		ritemDiv.id =`task${ib+1}`
 		ritemDiv.innerHTML = `<input ${datJs.status !="false"? "checked" : ""} type="checkbox"  id="task${ib+1}" class="checkmark" id="checkmark"> <label for="task${ib+1}" class="${datJs.status == "true" ? "infoLine" : ""}" >${datJs.name.slice(0, 26)}...</label> <div class="controls ">
 					<span class="fa-light fa-copy copy"></span>
+					<div class="fa-light fa-pen pen" id="pen"></div>
 					<i class="fa-light fa-trash-can-xmark delete"></i>
 				</div>`
 		resultTobox.appendChild(ritemDiv)
@@ -57,7 +106,6 @@ function ritem3() {
 resultTobox.addEventListener("click", (e)=>{
 	if(e.target.nodeName == "LABEL"){
 		e.target.classList.toggle("infoLine")
-		console.log(e.target.previousElementSibling)
 		datJs1 = JSON.parse(localStorage.getItem(`${e.target.parentNode.id}`))
 
 		if(e.target.previousElementSibling.checked ==false){
@@ -83,20 +131,150 @@ resultTobox.addEventListener("click", (e)=>{
 	}else if(e.target.nodeName=="SPAN"){
 		datJs1 = JSON.parse(localStorage.getItem(`${e.target.parentNode.parentNode.id}`))
 		navigator.clipboard.writeText(datJs1.name);
-		console.log(e.target.parentNode.parentNode.textContent)
 		swal({
+			timer:2000,
  			 icon: "success",
  			 text:"Text Copy"
 		});
 	}else if(e.target.nodeName == 'I'){
-		alert("List O`chirilyapti")
-		e.target.parentNode.parentNode.remove()
-		console.dir(e.target.parentNode.parentNode.id)
+		swal({
+			timer:2000,
+ 			 icon: "warning",
+ 			 text:"List O'chirlyapti....",
+ 			 closeModal:false,
+ 			 button: false
+		});
+		setTimeout(()=>{
+			swal({
+				timer:2000,
+ 			 icon: "success",
+ 			 text:"List O'chirildi"
+		});
+			e.target.parentNode.parentNode.remove()
 		localStorage.removeItem(`${e.target.parentNode.parentNode.id}`)
+		lenI1()
+		},1000)
+		
+
+	}else if(e.target.id == "pen"){
+		let editBox = document.querySelector(".editBox")
+			backBtn = document.querySelector(".backBtn")
+			editInput = document.querySelector(".editInput")
+			saveBtn= document.querySelector(".saveBtn")
+		editBox.style.transform = 'translateY(0)'
+
+		datJs1 = JSON.parse(localStorage.getItem(`${e.target.parentNode.parentNode.id}`))
+		editInput.value = datJs1.name
+
+		
+
+		saveBtn.addEventListener("click",()=>{
+			taskinfo = {id:datJs1.id, name:editInput.value.trim(), status:datJs1.status}
+			localStorage.setItem(`${e.target.parentNode.parentNode.id}`, JSON.stringify(taskinfo))
+			console.log()
+			e.target.parentNode.previousElementSibling.textContent = editInput.value.slice(0, 26) +"..."
+			editBox.style.transform = 'translateY(-100%)'
+			swal({
+				timer:2000,
+ 			 icon: "success",
+ 			 text:"Successfully Change To Do list"
+		});
+		})
+
+		backBtn.addEventListener("click", ()=>{
+			editBox.style.transform = 'translateY(-100%)'
+		})
 	}
 })
+clearAll.addEventListener("click", ()=>{
+	info = document.querySelectorAll('.item')
+	
+		swal({	
+		timer:2000,
+ 			 icon: "warning",
+ 			 text:"List O'chirlyapti....",
+ 			 closeModal:false,
+ 			 button: false
+		});
+		setTimeout(()=>{
+			swal({
+				timer:2000,
+	 			 icon: "success",
+	 			 text:"List O'chirildi"
+			});
+			
+			info.forEach( function(element, index) {
+				element.remove()
+				localStorage.removeItem(`${element.id}`)
+			});
 
 
+			lenI1()
+		},1000)
+
+	
+
+})
+function lenI1() {
+	let lenI = document.querySelector(".lenI")
+	lenI.textContent = localStorage.length-1
+}
+function setPass(){
+	let passRIn = document.querySelector(".pass")
+	let passRInA = document.querySelector(".passA")
+	if(passRIn.value == passRInA.value){
+		password={pass:passRIn.value}
+		localStorage.setItem("user", JSON.stringify(password) )
+		swal({
+			timer:2000,
+			icon:"success",
+			text:"Pin Code O'rnatildi!!!"
+		});
+		setTimeout(()=>{
+			document.querySelector('.loaderBox').style.transform = "translateY(-100%)"
+		},1500)
+	}else{
+		swal({
+			timer:2000,
+			icon:"warning",
+			text:"Ikkala Parol ham bir xil bo`lishi kerak!!!"
+		});
+	}
+}
+
+function logPass(){
+	dataLog = JSON.parse(localStorage.getItem("user"))
+	let passLIn = document.querySelector(".pass1")
+	if(passLIn.value == dataLog.pass){
+		swal({
+			timer:2000,
+			icon:"success",
+			text:"Saytga kirlyapti..."
+		});
+		setTimeout(()=>{
+			document.querySelector('.loaderBox').style.transform = "translateY(-100%)"
+		},1500)
+	}else{
+		swal({
+			timer:2000,
+			icon:"warning",
+			text:"Pin Code Xato"
+		});
+	}
+}
+function logorreg(){
+	passwordBoxR = document.querySelectorAll(".password")
+	passwordBoxL = document.querySelector(".password1")
+	dataLog = JSON.parse(localStorage.getItem("user"))
+	if(dataLog.pass !=""){
+		passwordBoxR[0].remove()
+	}else{
+		passwordBoxL.remove()
+	}
+
+	}
+logorreg()
+lenI1()
 ritem3()
 
 
